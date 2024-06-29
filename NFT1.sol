2f20c95323677299ef5272aa40585323fa8f2a0f
Daniel Tseke <101358046+Toritsemotse@users.noreply.github.com>
2024-06-29 17:27:27 +0100

    Add files via upload
    updated smart-contract

---
+++ NFT1.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract MyNFT {
+  // NFT properties (name, description, etc.)
+  string public name;
+  string public description;
+  // ...
+
+  // Total supply of NFTs (optional)
+  uint256 public totalSupply;
+  // Current number of minted NFTs
+  uint256 public mintedCount;
+
+  // Mapping to store owner of each NFT (tokenId => address)
+  mapping(uint256 => address) public ownerOf;
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor(string memory _name, string memory _description, uint256 _totalSupply) {
+    name = _name;
+    description = _description;
+    totalSupply = _totalSupply; // Set total supply if applicable
+  }
+
+  // Function to mint an NFT (payable if there's a price)
+  function mintNFT(address to) public {
+    // Restrict minting based on total supply (if applicable)
+    require(mintedCount < totalSupply, "NFT mint limit reached");
+    // Mint the NFT and assign ownership
+    mintedCount++;
+    balanceOf[to] = mintedCount;
+    ownerOf[mintedCount] = to;
+  }
+}
